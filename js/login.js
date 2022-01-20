00a2472fbadfd43fd6077f50b0bd422a19861d9b
Daniel Hernandez <danielhernandez09012002@gmail.com>
2022-01-19 23:04:33 -0600

    arreglos en el footer

---
+++ js/login.js
@@ -0,0 +1,11 @@
+// class usuario {
+//     constructor (mail, contrasenia) {
+//         this.mail = mail;
+//         this.contrasenia = contrasenia;
+//     }
+//     mostrar() {
+//         alert("Tus datos son " + this.mail + " " + this.contrasenia);
+//     }
+// }
+// const usuario1 = new usuario(prompt("Ingrese su mail"), prompt("Ingrese su contraseña"))
+// usuario1.mostrar()
