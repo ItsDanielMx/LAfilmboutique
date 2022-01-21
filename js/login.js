508b81e735770fa05621f4a95ed7b3064d1d1ea9
Daniel Hernandez <danielhernandez09012002@gmail.com>
2022-01-21 01:02:26 -0600

    login java

--- js/login.js
+++ js/login.js
@@ -1,11 +1,11 @@
-// class usuario {
-//     constructor (mail, contrasenia) {
-//         this.mail = mail;
-//         this.contrasenia = contrasenia;
-//     }
-//     mostrar() {
-//         alert("Tus datos son " + this.mail + " " + this.contrasenia);
-//     }
-// }
-// const usuario1 = new usuario(prompt("Ingrese su mail"), prompt("Ingrese su contraseña"))
-// usuario1.mostrar()
+class usuario {
+    constructor (mail, contrasenia) {
+        this.mail = mail;
+        this.contrasenia = contrasenia;
+    }
+    mostrar() {
+        alert("Tus datos son " + this.mail + " " + this.contrasenia + " bienvenid@.");
+    }
+}
+const usuario1 = new usuario(prompt("Ingrese su mail"), prompt("Ingrese su contraseña"))
+usuario1.mostrar()
