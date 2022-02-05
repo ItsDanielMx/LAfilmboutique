e088d1615977e43af2418368562492743ee31a94
Daniel Hernandez <danielhernandez09012002@gmail.com>
2022-02-05 13:05:34 -0600

    DOM

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let titulo = document.getElementById("titulo");
+titulo.innerText = "Probando el DOM";
+
+let subtitulo = document.getElementById("subtitulo");
+subtitulo.innerText = "Esto es maravilloso";
