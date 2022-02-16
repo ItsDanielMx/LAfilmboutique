72f1ebfbb0f32211f73e1ba48760d9910991e277
Daniel Hernandez <danielhernandez09012002@gmail.com>
2022-02-15 21:19:53 -0600

    carrito de compras

--- js/index.js
+++
@@ -1,5 +0,0 @@
-let titulo = document.getElementById("titulo");
-titulo.innerText = "Probando el DOM";
-
-let subtitulo = document.getElementById("subtitulo");
-subtitulo.innerText = "Esto es maravilloso";
